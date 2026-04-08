875a03e24dc4be70af5093425fba134cd3e3e535
NittramD <mi.diazgu@duocuc.cl>
2026-04-08 09:20:30 -0400

    cambio

--- src/main/java/com/example/cert1/Cert1Application.java
+++ src/main/java/com/example/cert1/Cert1Application.java
@@ -2,7 +2,7 @@ package com.example.cert1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class TareasApplication {
     public static void main(String[] args) {
